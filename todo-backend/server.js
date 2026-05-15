3e006abf5a41e34e1d32a6f92f3761165eadcafc
Sarthak <arkhadesarthak2477@gmail.com>
2026-05-15 16:42:14 +0530

    fixed cors

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://to-do-webpage-sa.netlify.app"
+}));
 app.use(express.json());
 
 // Routes
@@ -30,4 +32,3 @@ mongoose.connect(process.env.MONGO_URI)
     console.error('Failed to connect to MongoDB', err);
   });
 
-// Triggering nodemon restart!!
